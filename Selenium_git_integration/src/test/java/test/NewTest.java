2526ec85e5a00f5152540e3e5bb80391d289520c
Dipanshu <Dipanshuonline@gmail.com>
2023-06-18 08:47:25 +0530

    selenium_integration

---
+++ Selenium_git_integration/src/test/java/test/NewTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        // Set up the WebDriver configuration
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+      
+        driver.get("https://www.google.com");
+
+        // Perform actions on the page
+        // ...
+
+        // Add your assertions to verify expected results
+        // ...
+
+        // Example assertion
+        String pageTitle = driver.getTitle();
+        assert pageTitle.contains("Google") : "Google homepage is not displayed";
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Close the WebDriver
+        driver.quit();
+    }
+}
