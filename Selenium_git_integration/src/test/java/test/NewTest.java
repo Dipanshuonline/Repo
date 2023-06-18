5f4ca76ac48570aadef2874d96855805f06d48ef
Dipanshu Sharma <63792712+Dipanshuonline@users.noreply.github.com>
2023-06-18 09:12:20 +0530

    Update NewTest.java

--- Selenium_git_integration/src/test/java/test/NewTest.java
+++ Selenium_git_integration/src/test/java/test/NewTest.java
@@ -12,7 +12,7 @@ public class NewTest {
     @BeforeClass
     public void setUp() {
         // Set up the WebDriver configuration
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "Selenium_git_integration/drivers/chromedriver.exe");
         driver = new ChromeDriver();
     }
 
